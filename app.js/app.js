029a41ee4b62d13dbf36ad61128372ffedff43ec
Marco Alarcon <markinsh0@gmail.com>
2023-09-25 22:20:33 -0500

    Ejercicio Semana 11

---
+++ app.js/app.js
@@ -0,0 +1,14 @@
+// const links = document.querySelectorAll('.navbar-nav a');
+// links.forEach(link => {
+//     link.addEventListener('click', ()=> {
+//         QuitarClaseActivo();
+//     });
+// });
+
+// function QuitarClaseActivo() {
+//     const linkes = document.querySelectorAll('.navbar-nav a');
+//     linkes.forEach(link => {
+//         console.log('Hola');
+//         link.classList.remove('active');
+//     });
+// }
